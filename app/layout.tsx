a89d379894c01e10003fb926ee248f4bf07d2ecc
bertXu <453630401@qq.com>
2024-05-22 09:50:29 +0800

    修复网络图片破图问题

--- app/layout.tsx
+++ app/layout.tsx
@@ -73,6 +73,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <meta name="theme-color" media="(prefers-color-scheme: light)" content="#fff" />
       <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#000" />
       <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
+      <head>
+        <meta name="referrer" content="no-referrer"/>
+      </head>
       <body className="bg-white pl-[calc(100vw-100%)] text-black antialiased dark:bg-gray-950 dark:text-white">
         <ThemeProviders>
           <Analytics analyticsConfig={siteMetadata.analytics as AnalyticsConfig} />
